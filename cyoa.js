0c9a29ad633fe15411eb51b8bd9aeeb25d81c02c
Brandon Fitch <fitchbj@gmail.com>
2016-09-28 17:59:26 -0400

    Buttons working.

--- cyoa.js
+++ cyoa.js
@@ -1,63 +1,63 @@
 $(".game-start-btn").click(function(){
-  $('html').load("game_start.html");
+  $('body').load("game_start.html");
 });
 
 $(".deckhand-mess").click(function(){
-  $('html').load("deckhand_mess.html");
+  $('body').load("deckhand_mess.html");
 });
 
 $(".hold").click(function(){
-  $('html').load("hold.html");
+  $('body').load("hold.html");
 });
 
 $(".kitchen").click(function(){
-  $('html').load("kitchen.html");
+  $('body').load("kitchen.html");
 });
 
 $(".mid-ship").click(function(){
-  $('html').load("mid_ship.html");
+  $('body').load("mid_ship.html");
 });
 
 $(".engine-room").click(function(){
-  $('html').load("engine_room.html");
+  $('body').load("engine_room.html");
 });
 
 $(".gunroom").click(function(){
-  $('html').load("gunroom.html");
+  $('body').load("gunroom.html");
 });
 
 $(".boiler-room").click(function(){
-  $('html').load("boiler_room.html");
+  $('body').load("boiler_room.html");
 });
 
 $(".ammo-room").click(function(){
-  $('html').load("ammo_room.html");
+  $('body').load("ammo_room.html");
 });
 
 $(".captains-quarters").click(function(){
-  $('html').load("captains_quarters.html");
+  $('body').load("captains_quarters.html");
 });
 
 $(".deck").click(function(){
-  $('html').load("deck.html");
+  $('body').load("deck.html");
 });
 
 $(".shark-death").click(function(){
-  $('html').load("shark_death.html");
+  $('body').load("shark_death.html");
 });
 
 $(".end-win").click(function(){
-  $('html').load("end_win.html");
+  $('body').load("end_win.html");
 });
 
-$(".secret-btn-one").click(function(){
-  $('html').load("hold.html");
+$(".secret-btn-1").click(function(){
+  $('body').load("hold.html");
 });
 
 $(".secret-btn-two").click(function(){
-  $('html').load("end_win.html");
+  $('body').load("end_win.html");
 });
 
 $(".play-again").click(function(){
-  $('html').load("index.html");
+  $('body').load("index.html");
 });
