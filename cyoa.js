9fdddf8bf659ec7c539c8b6db24ad46574a0fe50
Brandon Fitch <fitchbj@gmail.com>
2016-09-28 16:32:50 -0400

    Most of the stages are working and have correct directions.  Few more.

--- cyoa.js
+++ cyoa.js
@@ -1,10 +1,61 @@
-// var begin = $('btn btn-default');
-//
-// begin
-// I want this to load(url) for game_start.html
+var body = $('body')
 
+$(".game-start-btn").click(function(){
+  body.load("game_start.html");
+});
 
+$(".deckhand-mess").click(function(){
+  body.load("/stages/deckhand_mess.html");
+});
 
-$(".btn").click(function(){
-  $('body').load("game_start.html");
+$(".hold").click(function(){
+  body.load("/stages/hold.html");
+});
+
+$(".kitchen").click(function(){
+  body.load("/stages/kitchen.html");
+});
+
+$(".mid-ship").click(function(){
+  body.load("/stages/mid_ship.html");
+});
+
+$(".engine-room").click(function(){
+  body.load("/stages/engine_room.html");
+});
+
+$(".gunroom").click(function(){
+  body.load("/stages/gunroom.html");
+});
+
+$(".boiler-room").click(function(){
+  body.load("/stages/boiler_room.html");
+});
+
+$(".ammo-room").click(function(){
+  body.load("/stages/ammo_room.html");
+});
+
+$(".captains-quarters").click(function(){
+  body.load("/stages/captains_quarters.html");
+});
+
+$(".deck").click(function(){
+  body.load("/stages/deck.html");
+});
+
+$(".shark-death").click(function(){
+  body.load("/stages/shark_death.html");
+});
+
+$(".end-win").click(function(){
+  body.load("/cyoa/end_win.html");
+});
+
+$(".secret-btn-1").click(function(){
+  body.load("/stages/hold.html");
+});
+
+(".secret-btn-2").click(function(){
+  body.load("/stages/end_win.html");
 });
